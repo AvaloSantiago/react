36507c47d9458a6034eeb5f24d76f9db4fab8071
AvaloSantiago <112868689+AvaloSantiago@users.noreply.github.com>
2023-09-15 16:04:33 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,65 @@
-import { useState } from 'react';
-import './App.css';
-import Card from './components/cards';
-
+import { useState } from "react";
+import Card from "./components/Cards";
+import "./App.css";
 function App() {
-  const [count, setCount] = useState(0);
+	const [cards, setCards] = useState([
+		{
+			id: 1,
+			imageUrl:
+				"https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+			title: "Bulbasaur",
+		},
+		{
+			id: 2,
+			imageUrl:
+				"https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png",
+			title: "Ivysaur",
+		},
+		{
+			id: 3,
+			imageUrl:
+				"https://images.wikidexcdn.net/mwuploads/wikidex/thumb/b/be/latest/20160309230456/Venusaur.png/800px-Venusaur.png",
+			title: "Venusaur",
+		},
+		{
+			id: 4,
+			imageUrl:
+				"https://images.wikidexcdn.net/mwuploads/wikidex/5/56/latest/20200307023245/Charmander.png",
+			title: "Charmander",
+		},
+		{
+			id: 5,
+			imageUrl:
+				"https://images.wikidexcdn.net/mwuploads/wikidex/thumb/f/fb/latest/20200411222755/Charmeleon.png/800px-Charmeleon.png",
+			title: "Charmeleon",
+		},
+		{
+			id: 6,
+			imageUrl:
+				"https://images.wikidexcdn.net/mwuploads/wikidex/thumb/9/95/latest/20160817212623/Charizard.png/640px-Charizard.png",
+			title: "Charizard",
+		},
+		// Agrega más tarjetas si es necesario
+	]);
 
-  return (
-    <div className="card-container">
-      <div className='planta'>
-      <Card imageUrl={"https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"} title={"Bulbasaur"} />
-      <Card imageUrl={"https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png"} title={"Ivysaur"} />
-      <Card imageUrl={"https://images.wikidexcdn.net/mwuploads/wikidex/thumb/b/be/latest/20160309230456/Venusaur.png/800px-Venusaur.png"} title={"Venusaur"}/>
-      </div>
-      <div className="fuego">
-      <Card imageUrl={"https://images.wikidexcdn.net/mwuploads/wikidex/5/56/latest/20200307023245/Charmander.png"} title={"Charmander"}/>
-      <Card imageUrl={"https://images.wikidexcdn.net/mwuploads/wikidex/thumb/f/fb/latest/20200411222755/Charmeleon.png/800px-Charmeleon.png"} title={"Charmeleon"}/>
-      <Card imageUrl={"https://images.wikidexcdn.net/mwuploads/wikidex/thumb/9/95/latest/20160817212623/Charizard.png/640px-Charizard.png"} title={"Charizard"}/>
-    </div>
-    </div>
-  );
+	const handleDeleteCard = (id) => {
+		// Filtrar las tarjetas para eliminar la que coincida con el ID
+		const updatedCards = cards.filter((card) => card.id !== id);
+		setCards(updatedCards);
+	};
+
+	return (
+		<div className="card-container">
+			{cards.map((card) => (
+				<Card
+					key={card.id}
+					imageUrl={card.imageUrl}
+					title={card.title}
+					onDelete={() => handleDeleteCard(card.id)} // Pasar la función de eliminación
+				/> 
+			))}
+		</div>
+	);
 }
 
 export default App;
