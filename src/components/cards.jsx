269eecc45846eb3b71cfef72e32524560489b2d7
AvaloSantiago <112868689+AvaloSantiago@users.noreply.github.com>
2023-10-27 15:29:17 -0300

    like y borrar, tambien css

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Card({ imageUrl, title }) {
+function Card({ imageUrl, title, onDelete }) {
+  const [likes, setLikes] = useState(0);
+
+  const handleLikeClick = () => {
+    setLikes(likes + 1);
+  };
+
+  const handleDeleteClick = () => {
+    // Llama a la función de eliminación proporcionada por el componente padre
+    onDelete();
+  };
+
   return (
     <div className="card">
       <img src={imageUrl} alt="" />
       <h1>{title}</h1>
+      <div className="likes">
+        <button onClick={handleLikeClick}>Like</button>
+        <span>{likes} Likes</span>
+      </div>
+      <button onClick={handleDeleteClick}>Eliminar</button>
     </div>
   );
 }
